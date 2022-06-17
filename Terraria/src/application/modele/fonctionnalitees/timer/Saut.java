67623dd78453552302807a2f21009a9994e32e58
MehediT <mtEteindre@gmail.com>
2022-06-17 17:14:37 +0200

    debug du merge

--- Terraria/src/application/modele/fonctionnalitees/timer/Saut.java
+++ Terraria/src/application/modele/fonctionnalitees/timer/Saut.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package application.modele.fonctionnalitees.timer;
 
 import java.util.TimerTask;
@@ -17,23 +16,3 @@ public class Saut extends TimerTask {
 		a.setSaut(false);
 	}
 }
-=======
-package application.modele.fonctionnalitees.timer;
-
-import java.util.TimerTask;
-
-import application.modele.Acteur;
-
-public class Saut extends TimerTask {
-
-	private Acteur a;
-	public Saut(Acteur a) {
-		this.a=a;
-	}
-
-	@Override
-	public void run() {
-		a.setSaut(false);
-	}
-}
->>>>>>> refs/heads/Fontionnnalité_Animation
